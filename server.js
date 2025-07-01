3e68143f1717736615c77c9d2241d48dad56b354
NiceDoggo <leonardotal2005@gmail.com>
2025-07-01 12:51:27 +0300

    better project base

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 import app from './app.js'
 import { port } from './config/index.js'
-import { connectToDB } from './config/db.js';
 
 app.listen(port, async () => {
     try {
-        await connectToDB();
         console.log(`Server is running on http://localhost:${port}`);
 
     } catch (error) {
--- utils/token.js
+++ utils/token.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import  {jwtKey} from '../config/index.js'
+import { api_key, api_secret } from '../config/index.js'
 
 
 export const createToken = async (payload, expiresIn = '24h') => {
@@ -8,3 +9,54 @@ export const createToken = async (payload, expiresIn = '24h') => {
 export const verifyToken = async(token) => {
     return await jwt.verify(token,jwtKey)
 } 
+
+let petfinderTokenData = null;
+export const fetchPetfinderToken = async () => {
+    const url = 'https://api.petfinder.com/v2/oauth2/token';
+    const params = new URLSearchParams({
+        grant_type: 'client_credentials',
+        client_id: api_key,
+        client_secret: api_secret
+    });
+
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: params.toString()
+    });
+
+    if (!res.ok) throw new Error(`Token fetch failed: ${res.status}`);
+    const data = await res.json();
+    console.log (`Token: ${data.access_token}`);
+    
+    petfinderTokenData = {
+        access_token: data.access_token,
+        expires_in: data.expires_in, 
+        timestamp: Date.now()
+    };
+    
+    return data;
+};
+
+export const isPetfinderTokenExpired = () => {
+    if (!petfinderTokenData) {
+        return true; 
+    }
+    
+    const currentTime = Date.now();
+    const tokenAge = currentTime - petfinderTokenData.timestamp;
+    const tokenLifetime = petfinderTokenData.expires_in * 1000; 
+    
+    const buffer = 5 * 60 * 1000;
+    
+    return tokenAge >= (tokenLifetime - buffer);
+};
+
+export const getPetfinderToken = async () => {
+    if (isPetfinderTokenExpired()) {
+        console.log("Petfinder token expired or missing, fetching new one...");
+        await fetchPetfinderToken();
+    }
+    
+    return petfinderTokenData.access_token;
+};
