a960ea909f36af840e4e7ad034a31239e7d14df9
maksim <maximzv005@gmail.com>
2025-07-05 16:26:19 +0300

    Improved users in data and adjusted the user.model

--- data/users.js
+++ data/users.js
@@ -1,9 +1,11 @@
-export const USERS = [
-    {
-        name: "or",
-        age:22,
-        credit:100
-    },
-   
-  
-]
+export const users = [
+  {
+    id: "1",
+    name: "Or",
+    email: "or@example.com",
+    city: "tel aviv",
+    age: 22,
+    password: "secret123",
+    pet_ids: []
+  }
+];
